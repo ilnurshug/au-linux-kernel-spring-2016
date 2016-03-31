a96986d9d459937671476cb2bcbcff7df3d2e3d9
Ilnur <itmo.stud@gmail.com>
2016-03-31 20:28:19 +0300

    ca1

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+long long t = 0;
+
 int main()
 {
-    // TODO
+	long ps = sysconf(_SC_PAGESIZE);
+	
+	size_t s = ps;
+	char* p = 0;
+	while (s > 1024)
+	{
+		p = (char*)malloc(s);
+		if (p) 
+		{
+			mmap(0, s, PROT_READ | PROT_WRITE, MAP_SHARED, 0, 0);
+			t += s;
+			s *= 2;
+		}
+		else
+		{
+			s /= 2;
+		}
+	}
+	
+	printf("%lld\n", t); 
+	
     return 0;
 }
